ede582ae0a038a68f5f8c0d26405870adfc2754b
Giovanna Nogueira <33638087+GnF17@users.noreply.github.com>
2021-10-25 13:23:10 -0300

    Update trabalho2.py

--- Trabalho 2 - A Batalha de Crait/trabalho2.py
+++ Trabalho 2 - A Batalha de Crait/trabalho2.py
@@ -1,11 +1,38 @@
-#Falta a parte da arvore
+#Falta arrumar a parte de imprimir os caminhos percorridos pela arvore
+
+class tree:
+  def __init__(self,rt, e = None, d = None):
+    self.root = rt
+    self.esq = e
+    self.dir = d
+
+  def mudarRoot(self, dado):
+    self.root = dado 
+
+def criar(dic, arvore):
+  if len(dic[1])>0:
+    if dic[1][0]=='.':
+      if arvore.esq==None:
+        arvore.esq=tree('*')
+      criar([dic[0],dic[1][1:]],arvore.esq)
+    else:
+      if arvore.dir==None:
+        arvore.dir=tree('*')
+      criar([dic[0],dic[1][1:]],arvore.dir)
+  else:
+    arvore.mudarRoot(dic[0])
+
+arvore = tree('*')
 
 n = int(input())
 dic = {}
+error = True
 
 for i in range(n):
   txt = input().split()
+  criar([txt[0],txt[1]],arvore)
   dic[txt[0]] = txt[1]
+
 
 escolha = int(input())
 if escolha==0:
@@ -34,6 +61,57 @@ while len(mensagem)>0:
         finalMessage += get_key(mensagem[0])
     except:
       print('Impossível decodificar a mensagem!')
+      finalMessage = ''
+      error = False
+      break
+  else:
+    try:
+      if mensagem[0]!=' ':
+        finalMessage += dic[mensagem[0]]+' '
+      else:
+        finalMessage += '/'
+    except:
+      print('Impossível codificar a mensagem!')
+      finalMessage = ''
+      error = False
+      break
+  mensagem.pop(0)
+
+print(finalMessage.strip())
+
+barra = ''
+altura = 0
+
+def alturA(a):
+  global barra
+  global altura
+
+  if len(barra)/2 > altura:
+    altura = len(barra)/2 -1
+  barra += '__'
+  if a != None:
+    alturA(a.esq)
+    alturA(a.dir)
+  barra = barra[:len(barra)-2]
+
+alturA(arvore)
+
+def mostrar(a, i):
+  global barra
+
+  if a != None:
+    if len(barra)/2==i:
+      print(a.root, end=' ')
+
+  barra += '__'
+  if a != None:
+    mostrar(a.dir, i)
+    mostrar(a.esq, i)
+  barra = barra[:len(barra)-2]
+
+if error:
+  for i in range(int(altura)):
+    mostrar(arvore, i)
       break
   else:
     try:
